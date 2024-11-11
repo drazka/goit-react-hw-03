03f2335f3d67778f8f1eb0bf9649febbcbcaac6b
drazka <izabdr@gmail.com>
2024-11-11 21:36:40 +0100

    step1

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,17 @@
 import clsx from "clsx";
 import css from "./Contact.module.css";
+import { IoMdPerson } from "react-icons/io";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
-const Contact = () => {
+const Contact = ({name, number}) => {
     return (
   <div className={css.contact}>
+    <div>
+    <p className={css.contactName}><IoMdPerson size="24" className={css.iconName} />{name}</p>
+    <p className={css.contactNumber}><BsFillTelephoneFill size="20" className={css.iconNumber} />{number}</p>
+    </div>
+
+    <button className={css.buttonDelete}>Delete</button>
   </div>
     );
   };
--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,11 +1,22 @@
 import clsx from "clsx";
 import css from "./ContactList.module.css";
+import contacts from '../../contacts.json';
+import Contact from '../contact/Contact'
 
 const ContactList = () => {
     return (
-  <div className={css.contactList}>
-  </div>
+  <ul className={css.contactList}>
+    {contacts.map(contact => (
+          <li key={contact.id}>
+            <Contact 
+              name={contact.name} 
+              number={contact.number} 
+            />
+          </li>
+        ))}
+  </ul>
     );
   };
   
   export default ContactList;
+
